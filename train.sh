247efb5804b89c60e02ac832af5f1bb6ac621ce8
lixc <364592279@qq.com>
2021-11-24 11:36:30 +0800

    initial commit

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@ export OMP_NUM_THREADS=1
 export CUDA_VISIBLE_DEVICES=0,1
 
 python3 -m torch.distributed.launch --nproc_per_node=2 --use_env train_mlm.py \
---train-file ../bert/gene_rank_2021Jun15.txt --val-file ../bert/gene_rank_test.txt -b 64 \
+--train-file train.txt --val-file test.txt -b 64 \
 --lr 1.0e-4 --epochs 48 -j 2 --max-len 256 --num_warmup_steps 10000 \
---output-dir checkpoint --dict gene_rank_2021Jun15.json --print-freq 100
+--output-dir checkpoint --dict gene_rank_2021may31_tokenizer.json --print-freq 100
 
